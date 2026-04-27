43481cd7f2d06f7e28ab654a60926401ffa4eeb9
7ayoub15 <SAMY82696@GMAIL.COM>
2026-04-27 11:57:30 +0100

    first commit

---
+++ src/main/java/net/ayoub/digitalbankingback/DigitalBankingBackApplication.java
@@ -0,0 +1,13 @@
+package net.ayoub.digitalbankingback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingBackApplication.class, args);
+    }
+
+}
